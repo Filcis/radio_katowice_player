33670001b48144aebcca10012764dcf60409663d
Filcis <cislakfilip@gmail.com>
2018-08-10 12:06:27 +0200

    Added react-granim and Granim instance in the background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import Player from './Player';
 import logo from './logo.svg';
+import Granim from 'react-granim'
 import './App.css';
 
 class App extends Component {
   render() {
+
+var GranimConfig = {
+      states : {
+          "default-state": {
+              gradients: [
+                  ['#574444', '#000'],
+                  ['#000', '#574444'],
+                  ['#B81313', '#574444'],
+                  ['#574444','#B81313']
+              ],
+              transitionSpeed: 2000,
+              loop: true
+          },
+      }
+    }
+
+    var GranimStyle = {
+      position: 'absolute',
+display: 'block',
+width: '100%',
+height: '19em',
+top: 0,
+right: 0,
+bottom: 0,
+left: 0
+    }
+
     return (
-      <div className="App container">
+      <div className="App">
+        <Granim style={GranimStyle} states={GranimConfig.states} id="granim"></Granim>
+        <div className="container">
         <Player />
+        </div>
       </div>
     );
   }
